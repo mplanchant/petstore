c4720d17f329e7eab4102bef1602ea7ec27173c4
Matthew Planchant <matthew.planchant@virginmoney.com>
2020-03-09 14:17:07 +0000

    Implement POST.

--- src/main/java/com/petstore/web/api/PetsApiImpl.java
+++ src/main/java/com/petstore/web/api/PetsApiImpl.java
@@ -5,37 +5,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestController
 public class PetsApiImpl implements PetsApi {
 
-    public static final Pet FIDO = new Pet().id(1L).name("Fido").tag("dog").age(3);
-    public static final Pet TIDDLES = new Pet().id(2L).name("Tiddles").tag("cat").age(6);
+    private final String RESOURCE = "/pet";
+
+    private static final Pet FIDO = new Pet().id(1L).name("Fido").tag("dog").age(3);
+    private static final Pet TIDDLES = new Pet().id(2L).name("Tiddles").tag("cat").age(6);
+
+    private static List<Pet> pets = Stream.of(FIDO, TIDDLES).collect(Collectors.toList());
 
     @Override
-    public ResponseEntity<Void> createPets() {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<Void> createPets(@Valid Pet pet) {
+        // Don't persist the new pet
+        log.info("Creating new pet {}", pet);
+        return ResponseEntity.created(buildUri(pet.getId())).build();
     }
 
     @Override
     public ResponseEntity<List<Pet>> listPets(@Valid Integer limit) {
         log.info("Responding with all pets");
-        return ResponseEntity.ok(List.of(FIDO, TIDDLES));
+        return ResponseEntity.ok(pets);
     }
 
     @Override
-    public ResponseEntity<Pet> showPetById(String petId) {
-        if (petId.equals("1")) {
-            log.info("Responding with Fido");
-            return ResponseEntity.ok(FIDO);
-        } else if (petId.equals("2")) {
-            log.info("Responding with Tiddles");
-            return ResponseEntity.ok(TIDDLES);
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<Pet> showPetById(Long petId) {
+        return pets.stream()
+                .filter(p -> p.getId().equals(petId))
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    private URI buildUri(long petId) {
+        return UriComponentsBuilder
+                .fromPath(RESOURCE)
+                .path("/{pet-id}")
+                .buildAndExpand(petId)
+                .toUri();
     }
 }
